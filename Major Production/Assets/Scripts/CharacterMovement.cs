3802ec96a8e4f1a3d88612ed128fd28d9c87514e
MaxGuidry <mguidry7@outlook.com>
2018-01-04 10:36:26 -0600

    start of planet

---
+++ Major Production/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+
+    public GameObject currentPlanet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

1aa7199801b3c8a01501fd57feb8189e6660554a
Donray Williams <w.donray@yahoo.com>
2018-01-04 10:35:36 -0600

    Started Inventory (Not Working)

---
+++ Major Production/Assets/Scripts/InventoryBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjects;
+
+public class InventoryBehaviour : MonoBehaviour
+{
+    public Inventory inventory;
+
+    void Start()
+    {
+        if (inventory.InventoryCap <= 0)
+        {
+            Debug.LogWarning("Inventory Size must be Greater than 0");
+            inventory.InventoryCap = 1;
+        }
+    }
+
+    public void AddToInventory(Item newItem)
+    {
+        Debug.Log("Item Added: " + newItem.Type);
+
+        var currentItem = GetItem(newItem.Type);
+
+
+        
+    }
+
+    public void RemoveFromInventory(Item newItem)
+    {
+
+    }
+
+    public Item GetItem(ItemType type)
+    {
+        for (var i = 0; i < inventory.InventoryCap; i++)
+        {
+            if (inventory.CurrentInventory[i].Type == type)
+            {
+                return inventory.CurrentInventory[i];
+            }
+        }
+        var nullitem = new Item { Type = ItemType.None };
+        return nullitem;
+    }
+
+    private int FindFreeItemSlot()
+    {
+        for (var i = 0; i < inventory.InventoryCap; i++)
+        {
+            if (inventory.CurrentInventory[i].Type == ItemType.None)
+                return i;
+        }
+        return -1;
+    }
+}
---
+++ Major Production/Assets/Scripts/ScriptableObjects/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Inventory", menuName = "Item/Inventory")]
+    public class Inventory : ScriptableObject
+    {
+        public int InventoryCap = 10;
+
+        public List<Item> StartingInventory = new List<Item>();
+
+        public List<Item> CurrentInventory
+        {
+            get { return StartingInventory; }
+        }
+    }
+}

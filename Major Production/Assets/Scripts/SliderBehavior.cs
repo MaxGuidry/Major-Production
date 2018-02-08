e862ca24e344759ef4a778d0132d93b7f82b0579
MaxGuidry <mguidry7@outlook.com>
2018-02-08 12:46:41 -0600

    slider thingy

---
+++ Major Production/Assets/Scripts/SliderBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cloth;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+public class SliderBehavior : MonoBehaviour
+{
+    //public List<Slider> sliders = new List<Slider>();
+    public Slider slider;
+    public Object objectToChange;
+    private bool start;
+    void Start()
+    {
+        start = true;
+        slider.onValueChanged.Invoke(0);
+    }
+   
+    public void ChangeValue(string s)
+    {
+        
+            
+        
+        //List<string> vars = new List<string>();
+        Dictionary<string, FieldInfo> vars = new Dictionary<string, FieldInfo>();
+        List<FieldInfo> fields = getType(objectToChange).GetFields().ToList();
+        foreach (var fieldInfo in fields)
+        {
+            vars.Add(fieldInfo.Name, fieldInfo);
+        }
+
+        if(start)
+        {
+            start = false;
+            slider.value = (float)vars[s].GetValue(objectToChange);
+            return;
+        }
+        vars[s].SetValue(objectToChange, slider.value);
+    }
+    public System.Type getType(object o)
+    {
+        return o.GetType();
+    }
+}
---
+++ Major Production/Assets/Scripts/SliderLabel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderLabel : MonoBehaviour
+{
+
+    public Text text;
+    public string Label;
+
+    void Start()
+    {
+        Label = text.text;
+        UpdateLabel();
+        
+    }
+    public void UpdateLabel()
+    {
+        if (Label == "")
+            return;
+        text.text = Label + (int)GetComponent<Slider>().value;
+    }
+}

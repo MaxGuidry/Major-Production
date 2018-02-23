31ef5b9151e3edbce831318cf1e463dbc2828b86
Donray Williams <w.donray@yahoo.com>
2018-02-23 16:45:16 -0600

    UI Needs Review

--- Major Production/Assets/Scripts/MonoBehaviours/MainMenuUI.cs
+++ Major Production/Assets/Scripts/MonoBehaviours/MainMenuUI.cs
@@ -6,6 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public AudioClip MusicClip;
+    private Canvas MainCanvas;
+    public Canvas OptionsCanvas;
+    public Slider sensitivity;
+    public Toggle invertMouse;
+    private AudioSource _backSound;
+
+    private void Start()
+    {
+        MainCanvas = GetComponent<Canvas>();
+        MainCanvas.gameObject.SetActive(true);
+        OptionsCanvas.gameObject.SetActive(false);
+
+        _backSound = FindObjectOfType<AudioSource>();
+        _backSound.volume = 1;
+
+        sensitivity.value = InputMap.Sensititivity;
+        Debug.Log(InputMap.Sensititivity);
+        if (InputMap.Sensititivity < 0)
+            invertMouse.isOn = true;
+        if (MusicClip == null) return;
+        _backSound.clip = MusicClip;
+        _backSound.Play();
+    }
+
     public void LoadScene(string scene)
     {
         SceneManager.LoadScene(scene);
@@ -14,5 +39,43 @@ public class MainMenuUI : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void Options()
+    {
+        switch (MainCanvas.GetComponentInChildren<Transform>().gameObject.activeInHierarchy)
+        {
+            case true:
+                foreach (var child in MainCanvas.GetComponentsInChildren<Transform>())
+                {
+                    child.gameObject.SetActive(false);
+                }
+                OptionsCanvas.gameObject.SetActive(true);
+                break;
+            case false:
+                MainCanvas.gameObject.SetActive(true);
+                for (var i = 0; i < MainCanvas.transform.childCount; i++)
+                {
+                    MainCanvas.transform.GetChild(i).gameObject.SetActive(true);
+                    MainCanvas.transform.GetChild(i).GetChild(0).gameObject.SetActive(true);
+                }
+                OptionsCanvas.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    public void InvertMouse()
+    {
+        InputMap.Sensititivity *= -1;
+    }
+
+    public void MouseSensitivitySlider(float value)
+    {
+        InputMap.Sensititivity = value;
+    }
+
+    public void AudioSlider(float value)
+    {
+        _backSound.volume = value;
     }
 }

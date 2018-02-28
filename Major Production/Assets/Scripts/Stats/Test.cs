dbb0b24d89ca9517ba51ad02cfba87d85267eedc
Donray Williams <w.donray@yahoo.com>
2018-02-28 17:09:33 -0600

    Stats Broken and Wrong need meeting

---
+++ Major Production/Assets/Scripts/Stats/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Test : ScriptableObject
+{
+    public PlayerStat player;
+    public void TestArmorUpgrade(int amount)
+    {
+        player.ArmorStatUpgrade(amount);
+    }
+}

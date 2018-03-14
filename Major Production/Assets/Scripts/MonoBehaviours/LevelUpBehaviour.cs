8e5c91cfbf5f83304c088f008b20acb4710d4921
Donray Williams <w.donray@yahoo.com>
2018-03-14 16:18:34 -0500

    Cleaning up and started on level up

---
+++ Major Production/Assets/Scripts/MonoBehaviours/LevelUpBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpBehaviour : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void ShowUI()
+    {
+        panel.SetActive(!panel.activeInHierarchy);
+    }
+
+    public void UpgradeStat(object[] args)
+    {
+        if (args.Length < 2)
+            return;
+        if (args[1] as string == "DPad Horizontal")
+        {
+            Debug.Log("Horizontal");
+            ShowUI();
+        }
+        if (args[1] as string == "DPad Vertical")
+        {
+            Debug.Log("Vertical");
+            ShowUI();
+        }
+    }
+}

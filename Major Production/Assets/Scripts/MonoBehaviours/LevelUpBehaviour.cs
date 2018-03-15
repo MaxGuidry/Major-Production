baf8f29e82b00a0c3196367bf8f9ec63afd0c7af
Donray Williams <w.donray@yahoo.com>
2018-03-15 10:45:58 -0500

    pulled from art

--- Major Production/Assets/Scripts/MonoBehaviours/LevelUpBehaviour.cs
+++ Major Production/Assets/Scripts/MonoBehaviours/LevelUpBehaviour.cs
@@ -5,25 +5,66 @@ using UnityEngine;
 public class LevelUpBehaviour : MonoBehaviour
 {
     public GameObject panel;
+    private PlayerStatBehaviour playerStat;
 
+    void Start()
+    {
+        playerStat = FindObjectOfType<PlayerStatBehaviour>();
+    }
+
+    /// <summary>
+    ///     Checks if the panel is active and sets it accordlying
+    /// </summary>
     public void ShowUI()
     {
         panel.SetActive(!panel.activeInHierarchy);
     }
 
-    public void UpgradeStat(object[] args)
+    /// <summary>
+    ///     Used to start the coroutine
+    /// </summary>
+    public void StartUpgrade()
     {
-        if (args.Length < 2)
-            return;
-        if (args[1] as string == "DPad Horizontal")
+        StartCoroutine(UpgradeStat());
+    }
+
+    /// <summary>
+    ///     Runs until a stat is upgraded
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator UpgradeStat()
+    {
+        var StatUpGraded = false;
+        while (!StatUpGraded)
         {
-            Debug.Log("Horizontal");
-            ShowUI();
-        }
-        if (args[1] as string == "DPad Vertical")
-        {
-            Debug.Log("Vertical");
-            ShowUI();
+            if (Input.GetAxis("DPad Horizontal") == -1)
+            {
+                playerStat.stats.GetStat("PHealth").Value++;
+                ShowUI();
+                StatUpGraded = true;
+            }
+
+            if (Input.GetAxis("DPad Horizontal") == 1)
+            {
+                playerStat.stats.GetStat("PDamage").Value++;
+                ShowUI();
+                StatUpGraded = true;
+            }
+
+            if (Input.GetAxis("DPad Vertical") == 1)
+            {
+                playerStat.stats.GetStat("PArmor").Value++;
+                ShowUI();
+                StatUpGraded = true;
+            }
+
+            if (Input.GetAxis("DPad Vertical") == -1)
+            {
+                playerStat.stats.GetStat("PSpeed").Value++;
+                ShowUI();
+                StatUpGraded = true;
+            }
+            yield return null;
         }
     }
 }

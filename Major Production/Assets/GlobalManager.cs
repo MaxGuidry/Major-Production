1f1149837c86892bc3b459ed439c8c5a60abd28e
MaxGuidry <mguidry7@outlook.com>
2018-03-23 10:54:00 -0500

    fuck this

---
+++ Major Production/Assets/GlobalManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GlobalManager : MonoBehaviour {
+
+    void Awake()
+    {
+        if (FindObjectOfType<NetworkManager>() == null)
+            GLOBALS.SplitscreenOnline = false;
+        GLOBALS.SplitscreenLocal = true;
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

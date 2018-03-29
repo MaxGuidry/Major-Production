1e2fa54f5b16a1abc08c0a01070344f263137039
Donray Williams <w.donray@yahoo.com>
2018-03-29 10:49:39 -0500

    working after check 100%

--- Major Production/Assets/SplitScreenConnection.cs
+++ Major Production/Assets/SplitScreenConnection.cs
@@ -8,71 +8,43 @@ public class SplitScreenConnection : MonoBehaviour
 {
     public List<GameObject> Panels;
     public SplitScreenPlayers Players;
-    public GameEventArgs PlayerJoined;
-    //Get Joystick Names
     public List<string> connectedControllers;
-    private List<string> oldStrings;
+    private Dictionary<GameObject, string> m_Controllers;
+
     // Use this for initialization
     void Start()
     {
         connectedControllers = Input.GetJoystickNames().ToList();
-        oldStrings = connectedControllers;
         Players.PlayersJoined = connectedControllers.Count;
+        m_Controllers = new Dictionary<GameObject, string>();
         foreach (var i in Panels)
+        {
             i.gameObject.GetComponentInChildren<Text>().text = "Connect Controller";
+            m_Controllers.Add(i, "Connect Controller");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        connectedControllers = Input.GetJoystickNames().ToList();
-        if (oldStrings.Count != connectedControllers.Count)
-        {
-            PlayerJoined.Raise(this);
-        }
-        oldStrings = connectedControllers;
+        connectedControllers = Input.GetJoystickNames().ToList();        
         for (var i = 0; i < connectedControllers.Count; i++)
         {
-            if (connectedControllers[i] == "")
-            {
-                Panels[i].gameObject.GetComponentInChildren<Text>().text = "Connect Controller";
-                connectedControllers.Remove(connectedControllers[i]);
-                i -=1;
-            }
+            var val = "";
+            if (connectedControllers[i] != "")
+                val = "Player " + (i + 1) + " joined game";
+            else
+                connectedControllers.RemoveAt(i); 
+            m_Controllers[Panels[i]] = val;
+        }
 
-            if (connectedControllers[i].Contains("Controller"))
-            {
-                connectedControllers[i] += i.ToString();
-            }
-
-            if (connectedControllers[i].Contains(i.ToString()))
-            {
-                Panels[i].gameObject.GetComponentInChildren<Text>().text = "Player " + (i + 1) + " joined game";
-            }
+        foreach (KeyValuePair<GameObject, string> item in m_Controllers)
+        {
+            if (item.Value != "")
+                item.Key.GetComponentInChildren<Text>().text = item.Value;
+            else
+                item.Key.GetComponentInChildren<Text>().text = "Connect Controller";
         }
         Players.PlayersJoined = connectedControllers.Count;
-    }
-
-    public void PlayerJoinedRaise()
-    {
-        if (connectedControllers.Count > 0)
-        {
-            //Iterate over every element
-            for (var i = 0; i < connectedControllers.Count; ++i)
-            {
-                //Check if the string is empty or not
-                if (!string.IsNullOrEmpty(connectedControllers[i]))
-                {
-                    //Not empty, controller temp[i] is connected
-                    Debug.Log("Controller " + i + " is connected using: " + connectedControllers[i]);
-                }
-                else
-                {
-                    //If it is empty, controller i is disconnected
-                    //where i indicates the controller number
-                    Debug.Log("Controller: " + i + " is disconnected.");
-                }
-            }
-        }
     }
 }

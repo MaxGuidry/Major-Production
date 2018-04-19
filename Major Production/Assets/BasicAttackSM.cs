97d1d52be49373efd0431c89de98b271d8d4b3ab
MaxGuidry <mguidry7@outlook.com>
2018-04-19 14:32:37 -0500

    combat

--- Major Production/Assets/BasicAttackSM.cs
+++ Major Production/Assets/BasicAttackSM.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BasicAttackSM : StateMachineBehaviour
 {
-
+    public CharacterMovement player;
     public AudioClip Punch;
 
     public AudioSource source;
@@ -13,17 +13,20 @@ public class BasicAttackSM : StateMachineBehaviour
 	{
 	    source.clip = Punch;
         source.Play();
+	    player.state = CharacterMovement.PlayerState.Attacking;
 	}
 
+   
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
 	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+	    player.state = CharacterMovement.PlayerState.Running;
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
---
+++ Major Production/Assets/Scripts/PlayerCombat/FistColliderDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistColliderDamage : MonoBehaviour
+{
+    public Animator anim;
+
+    public CharacterMovement player;
+    //public PlayerStatBehaviour stats;
+	// Use this for initialization
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (player.state == CharacterMovement.PlayerState.Attacking)
+        {
+            var idamageable = other.gameObject.GetComponent<IDamageable>();
+            if (idamageable != null)
+            {
+                idamageable.TakeDamage(50);
+            }
+        }
+    }
+
+}

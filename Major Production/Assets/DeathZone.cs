2b24cfcfa47eed8e69247963f166ab26a67defb6
MaxGuidry <mguidry7@outlook.com>
2018-05-10 16:54:42 -0500

    death zones

---
+++ Major Production/Assets/DeathZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        var character = other.GetComponent<PlayerStatBehaviour>();
+        if (character == null)
+            return;
+        character.TakeDamage(900000000);
+    }
+}

8f739d703b5d67550afc152cd40a1f41046a749a
MaxGuidry <mguidry7@outlook.com>
2018-05-18 17:46:23 -0500

    cleanup code

--- Major Production/Assets/Scripts/MonoBehaviours/CharacterMovement.cs
+++ Major Production/Assets/Scripts/MonoBehaviours/CharacterMovement.cs
@@ -1,81 +1,75 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Advertisements;
-using UnityEngine.Events;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using Random = UnityEngine.Random;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SectionHeader("Camera")]
-
-    public float Sensitivity = 1;
-    public Transform cameraPivot;
-
-    [SectionHeader("Speeds")]
-    public float RunSpeed = 2;
-    public float WalkSpeed = 1;
-
-    [SectionHeader("Projectiles")]
-    public Transform HandToShoot;
-    public GameObject RocketPrefab;
-    public GameObject shieldPrefab;
-
-    [SectionHeader("Particles")]
-    public GameObject deathParticle;
-    public GameObject deathPuff;
-    public GameObject takeOff;
-    public GameObject landing;
-    public GameObject dustKickup;
-    public GameObject whirlwind;
-
-
-    public Animator anim;
-
-    private string PlayerNumber;
-    private Rigidbody rb;
-    private Vector3 velocity = Vector3.zero;
-    private bool grounded = false;
-    private bool jumping = false;
-
-    private Vector3 acceleration = Vector3.zero;
-    private Coroutine dash;
-    public Coroutine whirl;
-    [HideInInspector]
-    public float rocketCooldown = 0;
-    public float MaxRocketCooldown = 8;
-
-    public float whirlwindCooldown = 0;
-    public float MaxWhirlwindCooldown = 2;
-
-    public float dashCooldown = 0;
-    public float MaxDashCooldown = 2f;
-
-    public float shieldCooldown = 0;
-    public float MaxShieldCooldown = 3;
-    private void Awake()
-    {
-        rb = gameObject.GetComponent<Rigidbody>();
-
-    }
-
     public enum PlayerState
     {
         Attacking,
         Running,
         Defending,
         Dead,
-        None,
+        None
     }
-    [HideInInspector]
-    public PlayerState state;
+
+    private Vector3 acceleration = Vector3.zero;
+
+
+    public Animator anim;
     [SerializeField] private GameEventArgsObject BreakObject;
+    public Transform cameraPivot;
+    private Coroutine dash;
+
+    public float dashCooldown;
+
+    [SectionHeader("Particles")] public GameObject deathParticle;
+
+    public GameObject deathPuff;
+    public GameObject dustKickup;
+    private bool grounded;
+
+    [SectionHeader("Projectiles")] public Transform HandToShoot;
+
+    private bool jumping;
+    public GameObject landing;
+    public float MaxDashCooldown = 2f;
+    public float MaxRocketCooldown = 8;
+    public float MaxShieldCooldown = 3;
+    public float MaxWhirlwindCooldown = 2;
 
     private NetworkManager nm;
+
+    private string PlayerNumber;
+    private Rigidbody rb;
+
+    [HideInInspector] public float rocketCooldown;
+
+    public GameObject RocketPrefab;
+
+    [SectionHeader("Speeds")] public float RunSpeed = 2;
+
+    [SectionHeader("Camera")] public float Sensitivity = 1;
+
+    public float shieldCooldown;
+    public GameObject shieldPrefab;
+
+    [HideInInspector] public PlayerState state;
+
+    public GameObject takeOff;
+    private Vector3 velocity = Vector3.zero;
+    public float WalkSpeed = 1;
+    public Coroutine whirl;
+    public GameObject whirlwind;
+
+    public float whirlwindCooldown;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
     //private Dictionary<string, float> axisValues = new Dictionary<string, float>();
 
     private void Start()
@@ -83,8 +77,8 @@ public class CharacterMovement : MonoBehaviour
         state = PlayerState.None;
         var basm = anim.GetBehaviour<BasicAttackSM>();
 
-        basm.Punch = this.GetComponent<AudioSource>().clip;
-        basm.source = this.GetComponent<AudioSource>();
+        basm.Punch = GetComponent<AudioSource>().clip;
+        basm.source = GetComponent<AudioSource>();
         basm.player = this;
         var mssm = anim.GetBehaviour<MissileStrikeSM>();
         mssm.player = this;
@@ -109,8 +103,6 @@ public class CharacterMovement : MonoBehaviour
             case "P4":
                 PlayerNumber = "3";
                 break;
-
-
         }
 
         //foreach (var button in f)
@@ -131,42 +123,31 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-
-
-
         if (state == PlayerState.Dead)
             return;
         if (Input.GetAxis("Right Bumper" + PlayerNumber) > 0 && state != PlayerState.Attacking)
-        {
             if (rocketCooldown < 0)
             {
                 anim.SetTrigger("Rocket");
                 rocketCooldown = MaxRocketCooldown;
             }
-        }
-        
+
 
         if (Input.GetAxis("Left Bumper" + PlayerNumber) > .1f && state != PlayerState.Attacking)
-        {
-                anim.SetTrigger("Whirlwind");
-        }
+            anim.SetTrigger("Whirlwind");
 
 
         if (Input.GetAxis("Trigger" + PlayerNumber) < -.9f)
-        {
             if (dash == null)
                 dash = StartCoroutine(Dash());
-        }
 
         if (Input.GetAxis("Trigger" + PlayerNumber) > .9f && state != PlayerState.Defending && shieldCooldown <= 0)
-        {
             StartCoroutine(Shield());
-        }
         //if (Input.GetKeyDown(KeyCode.N) && PlayerNumber == "")
         //  SpawnOnOtherPlanet(FindObjectsOfType<PlanetBehaviour>()[Random.Range(0, 4)]);
         if (!cameraPivot)
             return;
-        object[] test = { this, "A" };
+        object[] test = {this, "A"};
         if (Input.GetKeyDown(KeyCode.Space))
             Jump(test);
 
@@ -181,8 +162,8 @@ public class CharacterMovement : MonoBehaviour
         var hor = Input.GetAxis("Horizontal" + PlayerNumber);
 
         acceleration = cameraPivot.transform.forward;
-        var afor = cameraPivot.transform.forward * ((vert < .1f && vert > -.1f) ? 0 : vert);
-        var aright = cameraPivot.transform.right * ((hor < .1f && hor > -.1f) ? 0 : hor);
+        var afor = cameraPivot.transform.forward * (vert < .1f && vert > -.1f ? 0 : vert);
+        var aright = cameraPivot.transform.right * (hor < .1f && hor > -.1f ? 0 : hor);
 
         acceleration = (afor + aright) * 10;
 
@@ -208,18 +189,16 @@ public class CharacterMovement : MonoBehaviour
         }
 
 
-
-        transform.position += (velocity * Time.deltaTime);
+        transform.position += velocity * Time.deltaTime;
         //rb.MovePosition(pos);
-        Quaternion to = Quaternion.FromToRotation(this.transform.forward, velocity.normalized) * this.transform.rotation;
-        transform.rotation = Quaternion.Slerp(this.transform.rotation, to, .085f);
+        var to = Quaternion.FromToRotation(transform.forward, velocity.normalized) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, to, .085f);
         // transform.Rotate(to.eulerAngles);
-        anim.SetFloat("Velocity", velocity.magnitude * Mathf.Sign(Vector3.Dot(this.transform.forward, velocity.normalized)));
+        anim.SetFloat("Velocity", velocity.magnitude * Mathf.Sign(Vector3.Dot(transform.forward, velocity.normalized)));
         //Debug.Log(InputManager.Controller());
 
         rocketCooldown -= Time.deltaTime;
     }
-
 
 
     public void Jump(object[] args)
@@ -233,15 +212,12 @@ public class CharacterMovement : MonoBehaviour
             //if (!grounded)
             //    return;
             RaycastHit rh;
-            Physics.Raycast(transform.position + (transform.up * .2f), -transform.up, out rh, 1, ~LayerMask.GetMask("Player"));
-            if (!rh.transform)
-            {
-                return;
-
-            }
+            Physics.Raycast(transform.position + transform.up * .2f, -transform.up, out rh, 1,
+                ~LayerMask.GetMask("Player"));
+            if (!rh.transform) return;
 
             StartCoroutine(jumpForce());
-            rb.AddForce(this.transform.up * 21, ForceMode.Impulse);
+            rb.AddForce(transform.up * 21, ForceMode.Impulse);
             anim.SetTrigger("Jump");
         }
 
@@ -252,7 +228,6 @@ public class CharacterMovement : MonoBehaviour
     public IEnumerator jumpForce()
     {
         yield return new WaitForSeconds(.1f);
-
     }
 
     public IEnumerator Shield()
@@ -260,7 +235,7 @@ public class CharacterMovement : MonoBehaviour
         shieldCooldown = MaxShieldCooldown;
         state = PlayerState.Defending;
 
-        var go = Instantiate(shieldPrefab, this.transform.position, transform.rotation, this.gameObject.transform);
+        var go = Instantiate(shieldPrefab, transform.position, transform.rotation, gameObject.transform);
         var stats = GetComponent<PlayerStatBehaviour>();
         stats.Armor += 75;
         switch (GetComponent<Transform>().tag)
@@ -279,13 +254,16 @@ public class CharacterMovement : MonoBehaviour
                 stats.stats.GetStat("PArmor 3").Value += 75;
                 break;
         }
-        float f = 3f;
+
+        var f = 3f;
         while (f > 0)
         {
             f -= Time.deltaTime;
-            go.transform.rotation = new Quaternion(Mathf.Sin(0.01f) * this.transform.up.x, Mathf.Sin(0.01f) * this.transform.up.y, Mathf.Sin(0.01f) * this.transform.up.z, Mathf.Cos(0.01f)) * go.transform.rotation;
+            go.transform.rotation = new Quaternion(Mathf.Sin(0.01f) * transform.up.x, Mathf.Sin(0.01f) * transform.up.y,
+                                        Mathf.Sin(0.01f) * transform.up.z, Mathf.Cos(0.01f)) * go.transform.rotation;
             yield return null;
         }
+
         Destroy(go);
         state = PlayerState.Running;
 
@@ -304,6 +282,7 @@ public class CharacterMovement : MonoBehaviour
                 stats.stats.GetStat("PArmor 3").Value -= 75;
                 break;
         }
+
         stats.Armor -= 75;
         while (shieldCooldown > 0)
         {
@@ -312,7 +291,8 @@ public class CharacterMovement : MonoBehaviour
             yield return null;
         }
     }
-    void OnCollisionEnter(Collision other)
+
+    private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.GetComponent<MeshCollider>())
         {
@@ -322,7 +302,7 @@ public class CharacterMovement : MonoBehaviour
         }
     }
 
-    void OnCollisionStay(Collision other)
+    private void OnCollisionStay(Collision other)
     {
         if (other.gameObject.GetComponent<Collider>())
         {
@@ -333,9 +313,9 @@ public class CharacterMovement : MonoBehaviour
             jumping = false;
         }
     }
-    void OnCollisionExit(Collision other)
-    {
 
+    private void OnCollisionExit(Collision other)
+    {
         if (other.gameObject.GetComponent<Collider>())
         {
             var mc = other.gameObject.GetComponent<MeshCollider>();
@@ -343,10 +323,9 @@ public class CharacterMovement : MonoBehaviour
                 return;
             grounded = false;
             jumping = true;
-
-
         }
     }
+
     public void HitObject(object[] args)
     {
         if (!anim)
@@ -355,29 +334,22 @@ public class CharacterMovement : MonoBehaviour
             return;
         if (args.Length < 2)
             return;
-        if (args[1] as string == "X" + PlayerNumber)//&& anim.GetCurrentAnimatorStateInfo(). )
-        {
+        if (args[1] as string == "X" + PlayerNumber) //&& anim.GetCurrentAnimatorStateInfo(). )
             anim.SetTrigger("AttackBasic");
-
-            //BreakObject.ObjRaise(this);
-        }
     }
 
     public void Raycastattack()
     {
         RaycastHit[] hits;
 
-        hits = Physics.RaycastAll(this.transform.position, this.transform.forward, 6, ~LayerMask.GetMask("Player"));
+        hits = Physics.RaycastAll(transform.position, transform.forward, 6, ~LayerMask.GetMask("Player"));
         foreach (var rayhit in hits)
         {
-            if (rayhit.transform.gameObject == this.gameObject)
+            if (rayhit.transform.gameObject == gameObject)
                 continue;
 
             var toplevel = rayhit.transform;
-            while (toplevel.parent != null)
-            {
-                toplevel = toplevel.parent;
-            }
+            while (toplevel.parent != null) toplevel = toplevel.parent;
 
             if (toplevel.transform.GetComponent<BreakableResourceBehaviour>() != null)
             {
@@ -391,34 +363,26 @@ public class CharacterMovement : MonoBehaviour
                 var HitObject = toplevel.gameObject.GetComponentInChildren<PlayerStatBehaviour>();
                 //HitObject.TakeDamage(90000);
                 return;
-
             }
-
         }
-
-
-
     }
 
     public IEnumerator Dash()
     {
-
         anim.SetTrigger("Dash");
         float timer = 0;
-        Vector3 forward = this.transform.forward;
+        var forward = transform.forward;
         while (timer < .5f)
         {
             //this magically worked on the first try to stop clipping through planet/trees.
-            float mod = .35f;
+            var mod = .35f;
             RaycastHit rh;
-            if (Physics.Raycast(this.transform.position + this.transform.up, this.transform.forward, out rh, 1.5f,
+            if (Physics.Raycast(transform.position + transform.up, transform.forward, out rh, 1.5f,
                 ~LayerMask.GetMask("Player")))
-            {
                 if (rh.transform.gameObject.GetComponent<MeshCollider>() != null)
                     mod = .1f;
-            }
             timer += Time.deltaTime;
-            this.transform.position += forward * mod;
+            transform.position += forward * mod;
             yield return null;
         }
 
@@ -428,6 +392,7 @@ public class CharacterMovement : MonoBehaviour
             dashCooldown -= Time.deltaTime;
             yield return null;
         }
+
         dash = null;
     }
 
@@ -438,14 +403,14 @@ public class CharacterMovement : MonoBehaviour
         //    Mathf.Cos(-0.2f));
         go.transform.localScale = Vector3.one * .3f;
         var rocketprojectile = go.GetComponent<RocketProjectile>();
-        rocketprojectile.PlayerWhoShotMe = this.transform.parent.gameObject.name;
+        rocketprojectile.PlayerWhoShotMe = transform.parent.gameObject.name;
         rocketprojectile.damage = gameObject.GetComponent<PlayerStatBehaviour>().Damage;
     }
 
     public void SpawnOnOtherPlanet(PlanetBehaviour p)
     {
         var children = p.gameObject.transform.GetComponentsInChildren<Transform>();
-        List<GameObject> spawnPoints = new List<GameObject>();
+        var spawnPoints = new List<GameObject>();
         foreach (var child in children)
         {
             if (child.gameObject.tag != "SpawnPoint")
@@ -454,29 +419,28 @@ public class CharacterMovement : MonoBehaviour
         }
 
         var tpLocation = spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position;
-        this.transform.position = tpLocation;
+        transform.position = tpLocation;
     }
 
     public IEnumerator Whirlwind()
     {
         whirlwindCooldown = MaxWhirlwindCooldown;
         var go = Instantiate(whirlwind, Vector3.zero, Quaternion.identity);
-        go.transform.parent = this.transform;
+        go.transform.parent = transform;
 
         while (whirlwindCooldown > 0)
         {
             go.transform.localEulerAngles = new Vector3(-90, 0, 0);
-            go.transform.position = this.transform.position;
+            go.transform.position = transform.position;
             whirlwindCooldown -= Time.deltaTime;
             yield return null;
         }
 
         anim.SetTrigger("WhirlwindExit");
-        
+
         yield return new WaitForSeconds(0.1f);
         whirl = null;
         Destroy(go);
-
     }
 
 
@@ -508,9 +472,9 @@ public class CharacterMovement : MonoBehaviour
         StartCoroutine(deathps());
     }
 
-    IEnumerator deathps()
+    private IEnumerator deathps()
     {
         yield return new WaitForSeconds(1.5f);
-        Instantiate(deathPuff, this.transform.position, this.transform.rotation);
+        Instantiate(deathPuff, transform.position, transform.rotation);
     }
 }

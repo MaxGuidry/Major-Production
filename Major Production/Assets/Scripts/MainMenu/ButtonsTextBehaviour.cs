176dbef6c8f7a87c6bee4b4f65aafa1a634d09fc
Donray Williams <w.donray@yahoo.com>
2018-05-23 00:52:12 -0500

    Added button editor and TextMesh

---
+++ Major Production/Assets/Scripts/MainMenu/ButtonsTextBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsTextBehaviour : MonoBehaviour {
+    public Font TextFont;
+    public Color TextColor;
+    public bool BestFit;
+    public int FontSize;
+    public Sprite ButtonSprite;
+    public Color Normal, Highlighted, Pressed;
+	// Update is called once per frame
+	void Update () {
+        foreach (var button in FindObjectsOfType<Button>())
+        {
+            var cb = button.colors;
+            cb.normalColor = Normal;
+            cb.highlightedColor = Highlighted;
+            cb.pressedColor = Pressed;
+            button.colors = cb;
+            var image = button.GetComponent<Image>();
+            image.sprite = ButtonSprite;
+            var text = button.GetComponentInChildren<Text>();
+            text.font = TextFont;
+            text.color = TextColor;
+            text.fontSize = FontSize;
+        }
+    }
+}
